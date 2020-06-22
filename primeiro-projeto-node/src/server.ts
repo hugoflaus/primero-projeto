4efc323380846d42b68bb2d03b8c2f0f986a28bd
Hugo Flausino <hugo.flausino@hotmail.com>
2020-06-22 06:39:07 -0300

    Configured eslint

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (resquest, response) => {
-  return response.json({ message: 'Hello GoStack !'});
-});
+app.get('/', (resquest, response) => response.json({ message: 'Hello GoStack !' }));
 
 app.listen('3333', () => {
   console.log('Server Started on port 3333');
