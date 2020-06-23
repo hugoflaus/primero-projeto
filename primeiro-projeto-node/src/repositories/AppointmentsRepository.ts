faf7d030419f874e38261865229d2600bb6e94c3
Hugo Flausino <hugo.flausino@hotmail.com>
2020-06-23 07:03:55 -0300

    DTO(data transfer object) concept applied

--- primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,11 @@
 import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
 
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppointmentsRepository {
   private appointments: Appointment[];
 
@@ -20,9 +25,8 @@ class AppointmentsRepository {
     return findAppoitment || null;
   }
 
-  public create(provider: string, date: Date): Appointment {
-    const appoitment = new Appointment(provider, date);
-
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appoitment = new Appointment({ provider, date });
     this.appointments.push(appoitment);
 
     return appoitment;
