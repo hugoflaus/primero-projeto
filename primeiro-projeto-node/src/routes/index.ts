3e1a502d1739f1a981297bdcb843e5483a6746a8
Hugo Flausino <hugo.flausino@hotmail.com>
2020-06-26 07:09:42 -0300

    Mapped user router

--- primeiro-projeto-node/src/routes/index.ts
+++ primeiro-projeto-node/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import appointmetsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmetsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
