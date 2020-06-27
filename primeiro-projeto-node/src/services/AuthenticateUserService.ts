3b628a21b89c0f09243768cacb29d8e86744ef36
Hugo Flausino <hugo.flausino@hotmail.com>
2020-06-27 09:00:12 -0300

    Generated token JWT

--- primeiro-projeto-node/src/services/AuthenticateUserService.ts
+++ primeiro-projeto-node/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import { parse } from 'date-fns';
 import User from '../models/User';
@@ -11,6 +12,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, '63d398d3c03a91788b93f25fb410c5af', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
