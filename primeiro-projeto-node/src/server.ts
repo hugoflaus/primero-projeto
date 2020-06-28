05272a1db623e54e62011e3153622f0fd5fa1fb8
Hugo Flausino <hugo.flausino@hotmail.com>
2020-06-28 15:44:01 -0300

    Created global tractive of erros Separates what is known error from an unexpected error caused by some data processing

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import uploadConfing from './config/upload';
+import AppError from './erros/AppError';
 
 import './database';
 
@@ -12,6 +14,22 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfing.directory));
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: err.message,
+  });
+});
 
 app.listen('3333', () => {
   console.log('Server Started on port 3333');
