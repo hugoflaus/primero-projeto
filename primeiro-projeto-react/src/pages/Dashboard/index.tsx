015a43db732b76602ef58dfd86f7475b015bf479
Hugo Flausino <hugo.flausino@hotmail.com>
2020-07-08 23:21:32 -0300

    Conect Api get repositories

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,62 +1,61 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRespository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore respositórios no Github</Title>
-      <Form action="">
-        <input placeholder="Digite o nome do respositório" />
+      <Form onSubmit={handleAddRespository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do respositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/8002171?s=460&u=5bf3d5734e383a0cd191217e0c759be228715a27&v=4"
-            alt="Hugo Flausino"
-          />
-          <div>
-            <strong>desafio-fundamentos-nodejs</strong>
-            <p>
-              Node.js, TypeScript utilizando o conceito de models, repositories
-              e services!
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/8002171?s=460&u=5bf3d5734e383a0cd191217e0c759be228715a27&v=4"
-            alt="Hugo Flausino"
-          />
-          <div>
-            <strong>desafio-fundamentos-nodejs</strong>
-            <p>
-              Node.js, TypeScript utilizando o conceito de models, repositories
-              e services!
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/8002171?s=460&u=5bf3d5734e383a0cd191217e0c759be228715a27&v=4"
-            alt="Hugo Flausino"
-          />
-          <div>
-            <strong>desafio-fundamentos-nodejs</strong>
-            <p>
-              Node.js, TypeScript utilizando o conceito de models, repositories
-              e services!
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img src={repository.owner.avatar_url} alt={repository.full_name} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
