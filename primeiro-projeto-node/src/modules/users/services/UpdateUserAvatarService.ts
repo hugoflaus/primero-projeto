a7b1f03ee11d2cb81507f9bae13b423b6f0de24e
Hugo Flausino <hugo.flausino@hotmail.com>
2020-10-08 11:21:26 -0300

    Using classTransformer

--- primeiro-projeto-node/src/modules/users/services/UpdateUserAvatarService.ts
+++ primeiro-projeto-node/src/modules/users/services/UpdateUserAvatarService.ts
@@ -16,7 +16,7 @@ class UpdateUserAvatarService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('Storage')
+    @inject('StorageProvider')
     private storageProvider: IStorageProvider,
   ) {}
 
